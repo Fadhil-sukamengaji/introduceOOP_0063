50c8f278f5f478492560c450a408994a77f5f570
FadhilM <mudzakifadhil@gmail.com>
2025-04-29 15:53:34 +0700

    menambahkan data

--- pertemuan7.cpp
+++ pertemuan7.cpp
@@ -6,7 +6,14 @@ public :
     int nim;
     string nama;
     float nilai;
+
+    void printData(){
+        cout << "NIM : " << nim << endl;
+        cout << "NAMA : " << nama << endl;
+        cout << "NILAI : " << nilai << endl;
+    }
 };
+
 int main(){
-    
+        
 }
